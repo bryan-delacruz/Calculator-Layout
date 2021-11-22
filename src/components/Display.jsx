abca856c3234338beccd9824ef6faa1a1f747465
Bryan De La Cruz <bryan.delacruza@gmail.com>
2021-11-22 12:39:45 -0500

    Se agrega los botones modo light y nocturno,

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,6 +4,12 @@ import styles from "../css/Display.module.css";
 const Display = () => {
   return (
     <div className={styles.display}>
+      <div className={styles.mode}>
+        <span className={styles.containerBtn}>
+          <button className={`${styles.modeBtn} ${styles.modeLight}`}>☼</button>
+          <button className={`${styles.modeBtn} ${styles.modeDark}`}>☽</button>
+        </span>
+      </div>
       <div className={styles.operation}>
         1<span className={styles.operador}> + </span>1
       </div>
